f967fc81174a04d105968bb1d5ef1d86b8aa6809
Stas <stastimofeew98@gmail.com>
2025-01-05 01:03:24 +0300

    change config.jsx

--- src/config.jsx
+++ src/config.jsx
@@ -1,6 +1,6 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-const API_URL = 'https://fortniteapi.io/v2/shop?lang=ru&date=2022-01-01'
+const API_URL = process.env.REACT_APP_API_URL
 
 export {
     API_KEY,
